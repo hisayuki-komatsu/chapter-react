60a7ffccb2b4c3a11248970bf0036097d47de5b8
hisa <hisayuki.k8@gmail.com>
2022-05-09 23:54:11 +0900

    add state

--- src/components/detailA.tsx
+++ src/components/detailA.tsx
@@ -1,7 +1,12 @@
+import { useLocation } from 'react-router-dom'
+
 export const DetailA = () => {
+  const { state } = useLocation() as { state: string }
+
   return (
     <div>
       <p>DetailA</p>
+      <p>親コンポーネントは {state} </p>
     </div>
   )
 }
